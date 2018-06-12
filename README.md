58c268db5f7656468bdcaed94b4fc2d58a862586
Ziming Dong <ziming.dong@sagebase.org>
2018-06-11 18:23:05 -0700

    scan status notify SNS only on "INFECTED". "SKIPPED" tag for files over size limit

--- README.md
+++ README.md
@@ -192,10 +192,12 @@ the table below for reference.
 | AV_DEFINITION_PATH | Path containing files at runtime | /tmp/clamav_defs | No |
 | AV_SCAN_START_SNS_ARN | SNS topic ARN to publish notification about start of scan | | No |
 | AV_SCAN_START_METADATA | The tag/metada indicating the start of the scan | av-scan-start | No |
+| AV_SCAN_MAX_FILE_SIZE_BYTES | The maximum size, in bytes, of a file that will be scanned. Files larger than this size will be tagged with `AV_STATUS_SKIPPED`.  Currently does not support values above 25000000 | 25000000 | No |
 | AV_STATUS_CLEAN | The value assigned to clean items inside of tags/metadata | CLEAN | No |
-| AV_STATUS_INFECTED | The value assigned to clean items inside of tags/metadata | INFECTED | No |
+| AV_STATUS_INFECTED | The value assigned to infected items inside of tags/metadata | INFECTED | No |
+| AV_STATUS_SKIPPED | The value assigned to skipped items inside of tags/metadata | SKIPPED | No |
 | AV_STATUS_METADATA | The tag/metadata name representing file's AV status | av-status | No |
-| AV_STATUS_SNS_ARN | SNS topic ARN to publish scan results (optional) | | No |
+| AV_STATUS_SNS_ARN | SNS topic ARN to publish scan results of INFECTED files. (optional) | | No |
 | AV_TIMESTAMP_METADATA | The tag/metadata name representing file's scan time | av-timestamp | No |
 | CLAMAVLIB_PATH | Path to ClamAV library files | ./bin | No |
 | CLAMSCAN_PATH | Path to ClamAV clamscan binary | ./bin/clamscan | No |
--- scan.py
+++ scan.py
@@ -112,6 +112,9 @@ def sns_start_scan(s3_object):
 def sns_scan_results(s3_object, result):
     if AV_STATUS_SNS_ARN is None:
         return
+    if result != AV_STATUS_INFECTED: #Only notify for infected files
+        return
+
     message = {
         "bucket": s3_object.bucket_name,
         "key": s3_object.key,
@@ -127,16 +130,23 @@ def sns_scan_results(s3_object, result):
     )
 
 
+def scan_object(s3_object):
+    if s3_object.content_length > AV_SCAN_MAX_FILE_SIZE_BYTES:
+        return AV_STATUS_SKIPPED
+
+    verify_s3_object_version(s3_object)
+    sns_start_scan(s3_object)
+    clamav.update_defs_from_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX)
+    file_path = download_s3_object(s3_object, "/tmp")
+    return clamav.scan_file(file_path)
+
+
 def lambda_handler(event, context):
     start_time = datetime.utcnow()
     print("Script starting at %s\n" %
           (start_time.strftime("%Y/%m/%d %H:%M:%S UTC")))
     s3_object = event_object(event)
-    verify_s3_object_version(s3_object)
-    sns_start_scan(s3_object)
-    file_path = download_s3_object(s3_object, "/tmp")
-    clamav.update_defs_from_s3(AV_DEFINITION_S3_BUCKET, AV_DEFINITION_S3_PREFIX)
-    scan_result = clamav.scan_file(file_path)
+    scan_result = scan_object(s3_object)
     print("Scan of s3://%s resulted in %s\n" % (os.path.join(s3_object.bucket_name, s3_object.key), scan_result))
     if "AV_UPDATE_METADATA" in os.environ:
         set_av_metadata(s3_object, scan_result)
